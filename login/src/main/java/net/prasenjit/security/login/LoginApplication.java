62437a08fcdefcd47b69c8fad10841a34d8ffc84
PRASENJIT <github@prasenjit.net>
2015-08-19 02:45:17 +0530

    modified

--- login/src/main/java/net/prasenjit/security/login/LoginApplication.java
+++ login/src/main/java/net/prasenjit/security/login/LoginApplication.java
@@ -3,12 +3,14 @@ package net.prasenjit.security.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 @SpringBootApplication
-public class LoginApplication implements ServletContextInitializer {
+public class LoginApplication extends WebMvcConfigurerAdapter implements ServletContextInitializer {
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
     }
@@ -17,5 +19,10 @@ public class LoginApplication implements ServletContextInitializer {
     public void onStartup(ServletContext servletContext)
             throws ServletException {
         servletContext.getSessionCookieConfig().setName("SESSIONID");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/oauth/confirm_access").setViewName("authorize");
     }
 }
